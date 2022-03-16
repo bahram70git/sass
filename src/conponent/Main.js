bfbe6217d7ca6bd67968b636ffee6544afbf3901
bahram70git <bahram70git@gmail.com>
2022-03-16 11:33:59 +0330

    Finish!

--- src/conponent/Main.js
+++ src/conponent/Main.js
@@ -1,4 +1,5 @@
-import '../sass/sass.scss';
+import '../sass/scss.scss';
+import '../sass/sass.sass';
 
 const Main = () => {
     return ( 
@@ -15,15 +16,18 @@ const Main = () => {
             </header>
 
             <main>
-                <h1>First Title</h1>
+                <h1 id='header1'>First Title</h1>
+                <h4>Custom media query</h4>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ipsum consequat nisl vel pretium lectus. Ultrices tincidunt arcu non sodales neque sodales ut etiam. In massa tempor nec feugiat nisl. Interdum varius sit amet mattis vulputate enim nulla aliquet. In metus vulputate eu scelerisque felis imperdiet proin. Est ultricies integer quis auctor. Aliquam nulla facilisi cras fermentum. Pellentesque pulvinar pellentesque habitant morbi tristique. Aenean et tortor at risus viverra adipiscing. Cursus vitae congue mauris rhoncus aenean vel elit scelerisque mauris. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget egestas. Non odio euismod lacinia at quis risus sed vulputate. Ornare lectus sit amet est placerat in egestas erat. Magna fermentum iaculis eu non.</p>
 
-                <h2>Second Title</h2>
-                <p>Facilisis sed odio morbi quis. Penatibus et magnis dis parturient montes nascetur. Nam libero justo laoreet sit amet cursus. Nulla at volutpat diam ut. Neque volutpat ac tincidunt vitae semper. Tempus egestas sed sed risus pretium quam vulputate dignissim. Morbi tristique senectus et netus. Non nisi est sit amet facilisis magna etiam tempor. Mi quis hendrerit dolor magna eget est. Dignissim cras tincidunt lobortis feugiat. Ac placerat vestibulum lectus mauris ultrices eros in cursus. Vitae congue mauris rhoncus aenean vel. Ut pharetra sit amet aliquam. Morbi leo urna molestie at. Arcu ac tortor dignissim convallis aenean et tortor at risus. Imperdiet dui accumsan sit amet. Rhoncus dolor purus non enim praesent elementum facilisis leo vel. Sagittis purus sit amet volutpat.</p>
+                <h2 id='header2'>Second Title</h2>
+                <p id='second_p'>Facilisis sed odio morbi quis. Penatibus et magnis dis parturient montes nascetur. Nam libero justo laoreet sit amet cursus. Nulla at volutpat diam ut. Neque volutpat ac tincidunt vitae semper. Tempus egestas sed sed risus pretium quam vulputate dignissim. Morbi tristique senectus et netus. Non nisi est sit amet facilisis magna etiam tempor. Mi quis hendrerit dolor magna eget est. Dignissim cras tincidunt lobortis feugiat. Ac placerat vestibulum lectus mauris ultrices eros in cursus. Vitae congue mauris rhoncus aenean vel. Ut pharetra sit amet aliquam. Morbi leo urna molestie at. Arcu ac tortor dignissim convallis aenean et tortor at risus. Imperdiet dui accumsan sit amet. Rhoncus dolor purus non enim praesent elementum facilisis leo vel. Sagittis purus sit amet volutpat.</p>
             </main>
 
             <footer>
-                Footer Contents
+                <p>
+                    Footer Contents
+                </p>
             </footer>
         </div>
      );
